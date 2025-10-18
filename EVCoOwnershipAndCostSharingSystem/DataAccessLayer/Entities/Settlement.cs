380e9334526d4a5c7698a4db249d614334738b18
FostNg <frosting615@gmail.com>
2025-10-18 07:57:25 +0700

    Nop ca project
    Nop ca project

---
+++ EVCoOwnershipAndCostSharingSystem/DataAccessLayer/Entities/Settlement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Entities;
+
+public partial class Settlement
+{
+    public int SettlementId { get; set; }
+
+    public int AllocationId { get; set; }
+
+    public int PayerId { get; set; }
+
+    public int ReceiverId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime? PaymentDate { get; set; }
+
+    public string Method { get; set; } = null!;
+
+    public string? Reference { get; set; }
+
+    public virtual ExpenseAllocation Allocation { get; set; } = null!;
+
+    public virtual User Payer { get; set; } = null!;
+
+    public virtual User Receiver { get; set; } = null!;
+}
