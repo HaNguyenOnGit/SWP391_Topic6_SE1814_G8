948293be15aba269ed15e309e79b1bfa30e3b4cd
HaNguyenOnGit <dddd0848@gmail.com>
2025-10-23 13:27:22 +0700

    change config

--- src/back-end/EVCoOwnershipAndCostSharingSystem/DataAccessLayer/Entities/EvcoOwnershipAndCostSharingSystemContext.cs
+++ src/back-end/EVCoOwnershipAndCostSharingSystem/DataAccessLayer/Entities/EvcoOwnershipAndCostSharingSystemContext.cs
@@ -37,7 +37,7 @@ public partial class EvcoOwnershipAndCostSharingSystemContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:evcosystem.database.windows.net,1433;Database=EVCoOwnershipAndCostSharingSystem;Persist Security Info=False;User ID=evcosystem;Password=EV123456#;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:evcosystem.database.windows.net,1433;Initial Catalog=EVCoOwnershipDB;Persist Security Info=False;User ID=evcosystem;Password=EV123456#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
