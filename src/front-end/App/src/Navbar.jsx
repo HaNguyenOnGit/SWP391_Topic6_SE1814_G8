47a962e939e324ebf03729197671174067b2d15b
NguyenThanhTai <taintse185028@fpt.edu.vn>
2025-11-06 08:57:52 +0700

    Update Navbar.jsx

--- src/front-end/App/src/Navbar.jsx
+++ src/front-end/App/src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
-import { FaUserCircle, FaBars, FaTimes } from "react-icons/fa";
+import { FaUserCircle, FaBars, FaTimes, FaCar, FaCalendarAlt, FaHistory, FaBell, FaHeadset } from "react-icons/fa";
 import { useAuth } from "./auth/AuthContext";
 
 export default function Navbar() {
@@ -37,11 +37,11 @@ export default function Navbar() {
 
             <div className="nvbr-main">
                 <div className="navigate">
-                    <NavLink to="/vehicles">Xe của bạn</NavLink>
-                    <NavLink to="/schedule">Đặt lịch</NavLink>
-                    <NavLink to="/usageHistory">Lịch sử</NavLink>
-                    <NavLink to="/notifycaition">Thông báo</NavLink>
-                    <NavLink to="/support">Hỗ trợ</NavLink>
+                    <NavLink to="/vehicles"><span className="nav-icon"><FaCar /></span><span className="nav-label">Xe của bạn</span></NavLink>
+                    <NavLink to="/schedule"><span className="nav-icon"><FaCalendarAlt /></span><span className="nav-label">Đặt lịch</span></NavLink>
+                    <NavLink to="/usageHistory"><span className="nav-icon"><FaHistory /></span><span className="nav-label">Lịch sử</span></NavLink>
+                    <NavLink to="/notification"><span className="nav-icon"><FaBell /></span><span className="nav-label">Thông báo</span></NavLink>
+                    <NavLink to="/support"><span className="nav-icon"><FaHeadset /></span><span className="nav-label">Hỗ trợ</span></NavLink>
                 </div>
                 <div className="username" onClick={handleProfileClick}>
                     <FaUserCircle size={28} />
@@ -56,11 +56,11 @@ export default function Navbar() {
                         style={{ position: "absolute", top: 20, right: 20, cursor: "pointer" }}
                         onClick={() => setMenuOpen(false)}
                     />
-                    <NavLink to="/vehicles" onClick={() => setMenuOpen(false)}>Xe của bạn</NavLink>
-                    <NavLink to="/schedule" onClick={() => setMenuOpen(false)}>Đặt lịch</NavLink>
-                    <NavLink to="/usageHistory" onClick={() => setMenuOpen(false)}>Lịch sử</NavLink>
-                    <NavLink to="/notifycaition" onClick={() => setMenuOpen(false)}>Thông báo</NavLink>
-                    <NavLink to="/support" onClick={() => setMenuOpen(false)}>Hỗ trợ</NavLink>
+                    <NavLink to="/vehicles" onClick={() => setMenuOpen(false)}><span className="nav-icon"><FaCar /></span><span className="nav-label">Xe của bạn</span></NavLink>
+                    <NavLink to="/schedule" onClick={() => setMenuOpen(false)}><span className="nav-icon"><FaCalendarAlt /></span><span className="nav-label">Đặt lịch</span></NavLink>
+                    <NavLink to="/usageHistory" onClick={() => setMenuOpen(false)}><span className="nav-icon"><FaHistory /></span><span className="nav-label">Lịch sử</span></NavLink>
+                    <NavLink to="/notification" onClick={() => setMenuOpen(false)}><span className="nav-icon"><FaBell /></span><span className="nav-label">Thông báo</span></NavLink>
+                    <NavLink to="/support" onClick={() => setMenuOpen(false)}><span className="nav-icon"><FaHeadset /></span><span className="nav-label">Hỗ trợ</span></NavLink>
                     <div className="username" onClick={handleProfileClick}>
                         <FaUserCircle size={28} />
                         <span style={{ marginLeft: 8 }}>{username || "Tài khoản"}</span>
